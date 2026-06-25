f267fee344a83b0125089c931ea0ef4977ffaeca
PriyanshuKumarMorya <pk2784554@gmail.com>
2026-06-25 21:51:42 +0530

    Add files via upload

---
+++ ArrayOfSorted.java
@@ -0,0 +1,24 @@
+//import java.util.Scanner;
+//public class ArrayOfSorted {
+//    static boolean isSorted(int arr[],int X){
+//        boolean check = true;
+//        for(int i=0;i<arr.length;i++){
+//            if(arr[i]<arr[i-1]){
+//                check = false;
+//                break;
+//            }
+//        }
+//        return check;
+//    }
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter size of array : ");
+//        int n = sc.nextInt();
+//        int arr[] = new int[n];
+//        System.out.print("Enter "+n+" elements : ");
+//        for(int i=0;i<arr.length;i++){
+//            arr[i] = sc.nextInt();
+//        }
+//        System.out.println(isSorted(arr));
+//    }
+//}
