c61787a0d5b17deaa928596b794356d72c80dacf
PriyanshuKumarMorya <pk2784554@gmail.com>
2026-06-30 22:07:27 +0530

    Add files via upload

---
+++ FinalKeywordUse.java
@@ -0,0 +1,14 @@
+package Lecture;
+
+class Priyanshu6{
+    public final void display(){
+        System.out.println("Hello Everyone");
+    }
+}
+
+public class FinalKeywordUse {
+    public static void main(String[] args) {
+        Priyanshu6 p = new Priyanshu6();
+        p.display();
+    }
+}
