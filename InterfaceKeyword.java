40d64e91d342b05c5fc1416bfbdde67a0ac79c43
PriyanshuKumarMorya <pk2784554@gmail.com>
2026-07-01 12:16:24 +0530

    Add files via upload

---
+++ InterfaceKeyword.java
@@ -0,0 +1,21 @@
+package Lecture;
+
+interface Kumkum{
+    void display();
+    }
+interface kumkum1{
+     void fly();
+}
+class Kumkum3 implements  kumkum1{
+    public void fly(){
+        System.out.println("Hello world");
+    }
+
+}
+
+public class InterfaceKeyword {
+    public static void main(String[] args) {
+        
+    }
+
+}
